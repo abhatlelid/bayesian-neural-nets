b350d3eb4614dd47b88729d9fe5dfd257560fada
Anders Hatlelid <andebh@uio.no>
2022-10-05 07:42:40 +0200

    Data points now longer fixed

---
+++ scripts/data_example_mask.py
@@ -0,0 +1,118 @@
+
+# Imports 
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+#variables
+D = np.array(
+    [[1, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, -1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0,  1, -1, 0, 0, 0, 0, 0, 0],
+    [0, 0,  0, 1, -1, 0, 0, 0, 0, 0],
+    [0, 0,  0, 0, 1, -1, 0, 0, 0, 0],
+    [0, 0,  0, 0, 0, 1, -1, 0, 0, 0],
+    [0, 0,  0, 0, 0, 0, 1, -1, 0, 0],
+    [0, 0,  0, 0, 0, 0, 0, 1, -1, 0],
+    [0, 0,  0, 0, 0, 0, 0, 0, 1, -1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 1],]
+)
+sigma2_eps = 0.001
+n_param = D.shape[0]
+Q_m = D.T@D
+mu_m = np.zeros(n_param)
+Q_m_modified = Q_m + np.identity(10)*0.0001
+Sigma_m = np.linalg.inv(Q_m_modified)
+
+
+def get_sample_and_variables(N):
+    D = np.array(
+        [[1, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, -1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0,  1, -1, 0, 0, 0, 0, 0, 0],
+        [0, 0,  0, 1, -1, 0, 0, 0, 0, 0],
+        [0, 0,  0, 0, 1, -1, 0, 0, 0, 0],
+        [0, 0,  0, 0, 0, 1, -1, 0, 0, 0],
+        [0, 0,  0, 0, 0, 0, 1, -1, 0, 0],
+        [0, 0,  0, 0, 0, 0, 0, 1, -1, 0],
+        [0, 0,  0, 0, 0, 0, 0, 0, 1, -1],
+        [-1, 0, 0, 0, 0, 0, 0, 0, 0, 1],]
+    )
+    sigma2_eps = 0.001
+    n_param = D.shape[0]
+    Q_m = D.T@D
+    mu_m = np.zeros(n_param)
+    Sigma_eps = np.identity(n_param)*sigma2_eps
+    Q_m_modified = Q_m + np.identity(10)*0.0001
+    Sigma_m = np.linalg.inv(Q_m_modified)
+    Sigma_d = Sigma_m + Sigma_eps
+    d_sample = np.random.multivariate_normal(mu_m.flatten(), Sigma_d, size=N)
+    combined_sample = np.zeros((N, 2*n_param))
+    mask = np.zeros((N, n_param))
+    range_vector = np.zeros((N, n_param)) 
+    range_vector[:,] = np.arange((n_param))
+    n_masked = np.random.randint(n_param, size=N)
+    n_masked = n_masked.reshape((N, 1))
+    bool_vector = range_vector[:,] < n_masked
+    mask[bool_vector] = 1
+    np.random.shuffle(mask.T)
+    combined_sample[:,:n_param] = d_sample
+    combined_sample[:,n_param:] = mask
+
+    return combined_sample, Sigma_m, Q_m, sigma2_eps
+
+
+def get_input_tensor(d, x, n_param=10):
+    input_tensor = torch.zeros(n_param*2)
+    for d_, x_ in zip(d, x):
+        input_tensor[x_] = d_
+        input_tensor[n_param+x_] = 1
+    return input_tensor
+
+
+def get_posteior(d, x):
+    n_data = len(d)
+    Sigma_eps = np.identity(n_data)*sigma2_eps
+
+    G = np.zeros((n_data,n_param))
+    for idx, elem in enumerate(x):
+        G[idx, elem] = 1
+    mu_eps = G@mu_m
+
+    # Matrix calculations
+    Sigma_mm = Sigma_m
+    Sigma_dm = G@Sigma_mm
+    Sigma_md = Sigma_mm@(G.T)
+    Sigma_dd = G@Sigma_mm@(G.T) + Sigma_eps
+    Sigma_dd_inv = np.linalg.inv(Sigma_dd)
+    mu_m_d = mu_m + Sigma_md@Sigma_dd_inv@(d-mu_eps)
+    Sigma_m_d = Sigma_mm - Sigma_md@Sigma_dd_inv@Sigma_dm 
+    return mu_m_d, Sigma_m_d
+
+
+def plot_posterior(mu_m_d, Sigma_m_d):
+    # Plotting the posterior of m given d
+    sigma_m_d_ii = np.diagonal(Sigma_m_d)
+    m = range(0, 10)
+    plt.errorbar(
+        x = m,
+        y = mu_m_d, 
+        yerr=sigma_m_d_ii,
+        xerr=None,
+        fmt='o',
+        ecolor='k',
+        elinewidth=1.2, 
+        capsize=2,
+        linestyle='-',
+    )
+    plt.xlabel('m')
+    plt.ylabel('m|d')
+    plt.grid(color='silver', linestyle='-', linewidth=1)
+    plt.xticks(np.arange(len(m)))
+    plt.title('Posterior mean with posterior standard deviation')
+    plt.show()
+
+
+if __name__ == "__main__":
+    get_sample_and_variables(N=100)
--- scripts/train_mask.py
+++ scripts/train_mask.py
@@ -6,38 +6,42 @@ import torch
 from torch.linalg import multi_dot
 import matplotlib.pyplot as plt
 import datetime
+import torch.nn.functional as F
 
 import sys
 sys.path.append("..")
-from utils.model import Net
+from scripts.data_example_mask import get_sample_and_variables, get_input_tensor, get_posteior
+from utils.model_mask import Net_mask
 import scripts.data_example as de
 
 
 class Data(Dataset):
-    def __init__(self, d_sample):
-        self.data = torch.tensor(d_sample, dtype=torch.float)
+    def __init__(self, sample):
+        self.data = torch.tensor(sample, dtype=torch.float)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx], self.data[idx]
+        return self.data[idx]
 
 
-def custom_loss(output, target, Q_m, sigma2_eps, l2_lambda, model, G):
-    Psi = output
+def custom_loss(output, input_, Q_m, sigma2_eps, l2_lambda, model):
+    n_param = Q_m.shape[0]
+    d = input_[:,:n_param]
+    mask = input_[:,n_param:]
+    mask = torch.flatten(mask.to(torch.long))
+    ind = torch.nonzero(mask)
+    ind = torch.flatten(ind)
+    G = F.one_hot(ind, num_classes=n_param)
     G = torch.as_tensor(G, dtype=torch.float)
+    Psi = output
     GPsi = multi_dot([output, torch.transpose(G, 0, 1)])
-    print(GPsi.shape)
-    print(target.shape)
-    print()
-    print(GPsi.dtype)
-    print(target.dtype)
-    exit()
+    Gd = multi_dot([d, torch.transpose(G, 0, 1)])
 
 
     # Data loss
-    squared_error = torch.square(GPsi - target)
+    squared_error = torch.square(GPsi - Gd)
     squared_error_sum = torch.sum(squared_error, [0, 1])
     likelihood_loss = squared_error_sum/sigma2_eps
 
@@ -63,37 +67,41 @@ def custom_loss(output, target, Q_m, sigma2_eps, l2_lambda, model, G):
 
 if __name__ == "__main__":
     batch_size = 1
-    n_batches = 100
+    n_batches = 1000000
     N = batch_size*n_batches
 
-    data_indices, mu_m, Sigma_eps, G, mu_eps, Sigma_m, Q_m, Sigma_d, sigma2_eps = de.get_example_variables()
-    d_sample = np.random.multivariate_normal(G@mu_m.T.flatten(), Sigma_d, size=N)
+    #data_indices, mu_m, Sigma_eps, G, mu_eps, Sigma_m, Q_m, Sigma_d, sigma2_eps = de.get_example_variables()
+    #d_sample = np.random.multivariate_normal(G@mu_m.T.flatten(), Sigma_d, size=N)
+
+    combined_sample, Sigma_m, Q_m, sigma2_eps = get_sample_and_variables(N)
 
     n_epochs = 10
-    l2_lambda = 0.1
+    epoch_loss = np.zeros(n_epochs)
+    l2_lambda = 0.05
 
-    dataset = Data(d_sample)
+    dataset = Data(combined_sample)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     
-    model = Net()
+    model = Net_mask()
     lr = 1
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
     # loop over the dataset multiple times
     for epoch in range(n_epochs):
 
+        epoch_running_loss = 0
         running_loss = 0.0
-        for i, dat in enumerate(loader, 0):
+        for i, input_ in enumerate(loader, 0):
             # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = dat
+            #inputs, labels = dat
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = model(inputs)
+            outputs = model(input_)
 
-            loss = custom_loss(outputs, labels, Q_m, sigma2_eps, l2_lambda=l2_lambda, model=model, G=G)
+            loss = custom_loss(outputs, input_, Q_m, sigma2_eps, l2_lambda=l2_lambda, model=model)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10, norm_type=2.0, error_if_nonfinite=False)
             optimizer.step()
@@ -101,20 +109,40 @@ if __name__ == "__main__":
             # print statistics
             running_loss += loss.item()
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            epoch_running_loss += running_loss
             running_loss = 0.0
+        epoch_loss[epoch] = epoch_running_loss
+
+        
 
     print('Finished Training')
+
+    print(epoch_loss)
+    plt.plot(range(n_epochs), epoch_loss)
+    plt.show()
+
     time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    name = "../saved_models/model_weights_" + time + ".pth"
+    #name = "../saved_models_mask/model_weights_" + time + ".pth"
+    name = "../saved_models_mask/model_weights_mask.pth"
     torch.save(model.state_dict(), name)
 
     # Constructing example to plot when finished
-    d = np.array([8, -7, 0.01])
-    d = torch.tensor(d, dtype=torch.float)
-    out = model(d)
+    d = [1, -5, 3, 2, -1]
+    x = [2, 3, 4, 6, 9]
+
+    input_tensor = get_input_tensor(d, x)
+    out = model(input_tensor)
+
     with torch.no_grad():
         out = out.numpy().flatten()
 
-    plt.plot(range(0, 10), out)
-    plt.plot(data_indices, [out[i] for i in data_indices], 'bo')
+    post_mean, _ = get_posteior(d, x)
+    plt.plot(range(10), post_mean)
+    plt.plot(range(10), out)
+    plt.legend(["Analytical solution", "Neural net estimate"])
+    plt.plot(x, [out[i] for i in x], 'o')
+    plt.plot(x, [post_mean[i] for i in x], 'o')
+    plt.title("Analystical solution vs neural net estimate")
+    plt.xlabel("m")
+    plt.ylabel("m|d")
     plt.show()
